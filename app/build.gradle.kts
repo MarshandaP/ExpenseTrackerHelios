33f63bd8480f15957e0088c06fe74c0c19c935b9
dyleonn <dyleonuzzi@gmail.com>
2025-06-30 18:57:31 +0700

    CompileSDK 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.mardev.expensetrackerhelios"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mardev.expensetrackerhelios"
