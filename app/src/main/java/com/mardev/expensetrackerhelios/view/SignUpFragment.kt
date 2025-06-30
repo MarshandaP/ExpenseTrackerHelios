b32e680bc64fa896d82f1b11452d1f929cf6ff47
MarshandaP <120153912+MarshandaP@users.noreply.github.com>
2025-06-30 19:48:47 +0700

    SIGN IN & UP

--- app/src/main/java/com/mardev/expensetrackerhelios/view/SignUpFragment.kt
+++ app/src/main/java/com/mardev/expensetrackerhelios/view/SignUpFragment.kt
@@ -7,13 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.mardev.expensetrackerhelios.R
 
-class LoginFragment : Fragment() {
+class SignUpFragment : Fragment() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-
         }
     }
 
@@ -22,7 +21,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
     }
 
 }
