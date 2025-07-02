a87feef5aade76b79b8a085f63118d361b2e3ff5
MarshandaP <120153912+MarshandaP@users.noreply.github.com>
2025-07-02 12:52:57 +0700

    ...

--- app/src/main/java/com/mardev/expensetrackerhelios/view/BudgetListAdapter.kt
+++ app/src/main/java/com/mardev/expensetrackerhelios/view/BudgetListAdapter.kt
@@ -1,6 +1,34 @@
 package com.mardev.expensetrackerhelios.view
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mardev.expensetrackerhelios.databinding.BudgetListItemBinding
 import com.mardev.expensetrackerhelios.model.Budgeting
 
-class BudgetListAdapter(val budgetList: ArrayList<Budgeting>, val adapterOnClick : (Budgeting) -> Unit) {
+class BudgetListAdapter(val budgetList: ArrayList<Budgeting>) :
+    RecyclerView.Adapter<BudgetListAdapter.BudgetViewHolder>() {
+
+        class BudgetViewHolder(val binding: BudgetListItemBinding)
+            : RecyclerView.ViewHolder(binding.root)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BudgetViewHolder {
+            var binding = BudgetListItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent,false)
+            return BudgetViewHolder(binding)
+        }
+
+    override fun onBindViewHolder(holder: BudgetViewHolder, position: Int) {
+        holder.binding.budget = budgetList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return budgetList.size
+    }
+
+    fun updateBudgetlist(newBudgetList: List<Budgeting>){
+        budgetList.clear()
+        budgetList.addAll(newBudgetList)
+        notifyDataSetChanged()
+    }
 }
--- app/src/main/java/com/mardev/expensetrackerhelios/view/BudgetingFragment.kt
+++ app/src/main/java/com/mardev/expensetrackerhelios/view/BudgetingFragment.kt
@@ -5,16 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mardev.expensetrackerhelios.R
 import com.mardev.expensetrackerhelios.databinding.FragmentBudgetingBinding
+import com.mardev.expensetrackerhelios.model.Budgeting
 import com.mardev.expensetrackerhelios.viewmodel.ListBudgetViewModel
 
 
 class BudgetingFragment : Fragment() {
     private lateinit var binding: FragmentBudgetingBinding
     private lateinit var viewModel: ListBudgetViewModel
-    private val
+    val budgetListAdapter =  BudgetListAdapter(arrayListOf())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -31,10 +35,45 @@ class BudgetingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel =ViewModelProvider(this).get(ListBudgetViewModel::class.java)
+        viewModel.refresh()
+        binding.recBudgeting.layoutManager = LinearLayoutManager(context)
+        binding.recBudgeting.adapter = budgetListAdapter
+
         binding.btnBudget.setOnClickListener{
             val action = BudgetingFragmentDirections.actionNewBudget()
             Navigation.findNavController(it).navigate(action)
         }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel(){
+        viewModel.budgetLD.observe(viewLifecycleOwner, Observer {
+            budgetListAdapter.updateBudgetlist(it)
+            if(it.isEmpty()) {
+                binding.recBudgeting?.visibility = View.GONE
+                binding.txtError.setText("Your todo still empty.")
+            } else {
+                binding.recBudgeting?.visibility = View.VISIBLE
+            }
+        })
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == false) {
+                binding.progressLoad?.visibility = View.GONE
+            } else {
+                binding.progressLoad?.visibility = View.VISIBLE
+            }
+        })
+        viewModel.budgetLoadErrorLD.observe(viewLifecycleOwner, Observer {
+            if(it == false) {
+                binding.txtError?.visibility = View.GONE
+            } else {
+                binding.txtError?.visibility = View.VISIBLE
+            }
+        })
+
     }
 
 }
